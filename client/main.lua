8952d1d90645fce8c315cbe5623a8e5ea5ce978c
7evenStudios <122033280+7evenStudios@users.noreply.github.com>
2023-01-06 20:51:27 +0100

    Add files via upload

--- client/main.lua
+++ client/main.lua
@@ -7,10 +7,16 @@ end);
 
 RegisterNetEvent('7even_identity:nui:send', function(data)
   repeat Citizen.Wait(100) until isNuiReady;
+
+  if data.type == "open" then
+    TriggerEvent('esx_skin:resetFirstSpawn');
+  end
+
   SendNUIMessage(data);
 end);
 
 RegisterNUICallback('createCharacter', function(data)
+  TriggerEvent('esx_skin:playerRegistered');
   TriggerServerEvent('7even_identity:createCharacter', data);
 end);
 
