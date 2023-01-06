d229cdc7d753c091942bc5891765952688135bca
7evenStudios <122033280+7evenStudios@users.noreply.github.com>
2023-01-06 17:58:45 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author '7even Studios'
+description 'Identity [ESX]'
+version '1.0.0'
+
 shared_script 'config.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
